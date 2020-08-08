50862fe9aa614f657dde7e564c2b746d47f283b7
hnipun <hnipun@gmail.com>
2020-08-08 16:16:39 +0530

    alert update

--- alerts.py
+++ alerts.py
@@ -1,18 +1,26 @@
 import numpy as np
-from labml import tracker, experiment
+from labml import tracker, experiment, lab
+
+lab.configure({
+    'web_api': {
+        'url': 'https://api.lab-ml.com/api/v1/track?labml_token=67783400797e4068a0d2eb276fb82c9a&channel=training',
+        'frequency': 10,
+        'verify_connection': True
+    }
+})
 
 configs = {
-    'fs': 100,  # sample rate
-    'f': 2,  # the frequency of the signal
+    'fs': 100000,  # sample rate
+    'f': 1,  # the frequency of the signal
 }
 
 x = np.arange(configs['fs'])
 y = np.sin(2 * np.pi * configs['f'] * (x / configs['fs']))
 
-experiment.create(name='sign_wave')
+experiment.create(name='sin_wave')
 experiment.configs(configs)
 experiment.start()
 
 for y_i in y:
-    tracker.save({'y': y_i})
+    tracker.save({'loss': y_i, 'noisy': y_i + np.random.normal(0, 10, 100)})
     tracker.add_global_step()
