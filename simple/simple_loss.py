bc97eb876a3f1d23829ad025cfd4a23575366fc3
hnipun <hnipun@gmail.com>
2020-09-21 04:31:03 +0530

    simple_loss.py modifications

--- simple/simple_loss.py
+++ simple/simple_loss.py
@@ -1,18 +1,15 @@
-import numpy as np
-
+from numpy.random import random
 from labml import tracker, experiment
 
 conf = {'batch_size': 20}
-n = 0
 
 
-def train():
-    global n
-    n += 1
-    return 0.999 ** n + np.random.random() / 10, 1 - .999 ** n + np.random.random() / 10
+def train(n: int):
+    return 0.999 ** n + random() / 10, 1 - .999 ** n + \
+           random() / 10
 
 
-with experiment.record(name='sample', exp_conf=conf, web_api='samples', comment='test'):
+with experiment.record(name='sample', exp_conf=conf):
     for i in range(100000):
-        loss, accuracy = train()
+        loss, accuracy = train(i)
         tracker.save(i, {'loss': loss, 'accuracy': accuracy})
