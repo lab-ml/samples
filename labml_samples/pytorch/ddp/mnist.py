8659b5c00c09c41fe8269cdf419f0b0a91c6841b
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-11-20 21:54:27 +0530

    ddp

--- labml_samples/pytorch/ddp/mnist.py
+++ labml_samples/pytorch/ddp/mnist.py
@@ -11,14 +11,15 @@ def ddp_model(c: Configs):
     return DistributedDataParallel(Net().to(c.device), device_ids=[c.device])
 
 
-def main(rank, world_size, uuid):
-    torch.distributed.init_process_group("gloo", init_method="tcp://localhost:23456", rank=rank, world_size=world_size)
+def main(rank, world_size, uuid, init_method: str = 'tcp://localhost:23456'):
+    torch.distributed.init_process_group("gloo", init_method=init_method, rank=rank, world_size=world_size)
     conf = Configs()
-    experiment.create(uuid=uuid, name='cifar_10')
+    experiment.create(uuid=uuid, name='mnist ddp')
     experiment.distributed(rank, world_size)
     experiment.configs(conf,
                        {'optimizer.optimizer': 'Adam',
                         'optimizer.learning_rate': 1e-4,
+                        'model': 'ddp_model',
                         'device.cuda_device': rank})
     conf.set_seed.set()
     experiment.add_pytorch_models(dict(model=conf.model))
@@ -26,6 +27,20 @@ def main(rank, world_size, uuid):
         conf.run()
 
 
+def _launcher():
+    import os
+    world_size = int(os.environ['WORLD_SIZE'])
+    run_uuid = os.environ['RUN_UUID']
+    local_rank = int(os.environ['LOCAL_RANK'])
+    # print(world_size, run_uuid)
+    main(local_rank, world_size, run_uuid, 'env://')
+
+
 if __name__ == '__main__':
+    # Run single GPU
     # main(0, 1, experiment.generate_uuid())
-    torch.multiprocessing.spawn(main, args=(3, experiment.generate_uuid()), nprocs=3, join=True)
+
+    # Spawn multiple GPU
+    # torch.multiprocessing.spawn(main, args=(3, experiment.generate_uuid()), nprocs=3, join=True)
+
+    _launcher()
